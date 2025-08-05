c8e3f74032e68e526b6d96d6da045d4954e997a7
HasnanKhan <90119809+HasnanKhan@users.noreply.github.com>
2025-08-05 19:11:02 -0400

    Update Two_Sum.py

--- Arrays & Hashing/Two_Sum.py
+++ Arrays & Hashing/Two_Sum.py
@@ -1,1 +1,11 @@
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#Leetcode 1
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       x = {}
+       for i in range(len(nums)):
+        if target - nums[i] in x:
+            return [i, x[target - nums[i]]]
+        else:
+            x[nums[i]] = i
